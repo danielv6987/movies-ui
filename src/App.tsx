5abd6cb41beab76238cb136872d683f0f09c802c
Daniel Vecchi <danielv6987@gmail.com>
2026-01-19 16:37:05 -0500

    Added website layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,176 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { Dialog, DialogBackdrop, DialogPanel, TransitionChild } from '@headlessui/react';
+import { ArrowTrendingUpIcon, Bars3Icon, FireIcon, StarIcon, XMarkIcon, HomeIcon } from '@heroicons/react/24/outline';
+import TopRated from './TopRated.tsx';
+import Favorites from './Favorites.tsx';
+import Trending from './Trending.tsx';
+import Landing from './Landing.tsx';
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
-  const clientId = import.meta.env.VITE_CLIENT_ID;
-  const apiVersion = import.meta.env.VITE_API_VERSION;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [navigation, setNavigation] = useState([
+    { name: 'Home', href: '#', icon: HomeIcon, current: true },
+    { name: 'Top Rated', href: '#', icon: FireIcon, current: false },
+    { name: 'Trending', href: '#', icon: ArrowTrendingUpIcon, current: false },
+    { name: 'Favorites', href: '#', icon: StarIcon, current: false },
+  ]);
+  console.log('navigation:', navigation);
 
-  const getMovies = async () => {
-
-    const response = await fetch('https://api.trakt.tv/movies/popular',
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'trakt-api-key': clientId,
-          'trakt-api-version': apiVersion,
-        }
-      });
-    const data = await response.json();
-    console.log(data);
-    setMovies(data);
+  const updateNavigation = (name: string) => {
+    setNavigation(navigation.map((item) => ({ ...item, current: item.name === name })));
   }
-
-  useEffect(() => {
-  getMovies()
-  }, []);
 
   return (
     <>
-      {movies.length > 0 && movies.map((movie:any) => {
-        console.log("movie", movie)
-        return(
-          <div key={movie.title}>
-            <div>{movie.title}</div>
-            <div>testing</div>
-            {movie.images && movie.images.thumb.length > 0 && <img src={`https://${movie.images.thumb[0]}`} alt={movie.title} />}
+      <div>
+        {/* Mobile sidebar, show/hide based on sidebarOpen state. */}
+        <Dialog open={sidebarOpen} onClose={setSidebarOpen} className="relative z-50 lg:hidden">
+          <DialogBackdrop
+            transition
+            className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-closed:opacity-0"
+          />
+
+          <div className="fixed inset-0 flex">
+            <DialogPanel
+              transition
+              className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-closed:-translate-x-full"
+            >
+              <TransitionChild>
+                <div
+                  className="absolute top-0 left-full flex w-16 justify-center pt-5 duration-300 ease-in-out data-closed:opacity-0">
+                  <button type="button" onClick={() => setSidebarOpen(false)} className="-m-2.5 p-2.5">
+                    <span className="sr-only">Close sidebar</span>
+                    <XMarkIcon aria-hidden="true" className="size-6 text-white" />
+                  </button>
+                </div>
+              </TransitionChild>
+
+              {/* Sidebar component, swap this element with another sidebar if you like */}
+              <div
+                className="relative flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-2 ring-1 ring-white/10">
+                <button className="relative flex h-16 shrink-0 items-center" onClick={() => updateNavigation('Home')}>
+                  <img
+                    alt="Your Company"
+                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                    className="h-8 w-auto"
+                  />
+                </button>
+                <nav className="flex flex-1 flex-col">
+                  <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                    <li>
+                      <ul role="list" className="-mx-2 space-y-1">
+                        {navigation.map((item) => (
+                          <li key={item.name}>
+                            <a
+                              href={item.href}
+                              className={classNames(
+                                item.current
+                                  ? 'bg-white/5 text-white'
+                                  : 'text-gray-400 hover:bg-white/5 hover:text-white',
+                                'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
+                              )}
+                              onClick={() => updateNavigation(item.name)}
+                            >
+                              <item.icon aria-hidden="true" className="size-6 shrink-0" />
+                              {item.name}
+                            </a>
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </DialogPanel>
           </div>
-        )
-      })}
+        </Dialog>
+
+        {/* Static sidebar for desktop */}
+        <div className="hidden bg-gray-900 lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+          {/* Sidebar component, swap this element with another sidebar if you like */}
+          <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 px-6">
+            <button className="flex h-16 shrink-0 items-center" onClick={() => updateNavigation('Home')}>
+              <img
+                alt="Your Company"
+                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                className="h-8 w-auto"
+              />
+            </button>
+            <nav className="flex flex-1 flex-col">
+              <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                <li>
+                  <ul role="list" className="-mx-2 space-y-1">
+                    {navigation.map((item) => (
+                      <li key={item.name}>
+                        <a
+                          href={item.href}
+                          className={classNames(
+                            item.current ? 'bg-white/5 text-white' : 'text-gray-400 hover:bg-white/5 hover:text-white',
+                            'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
+                          )}
+                          onClick={() => updateNavigation(item.name)}
+                        >
+                          <item.icon aria-hidden="true" className="size-6 shrink-0" />
+                          {item.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+                <li className="-mx-6 mt-auto">
+                  <a
+                    href="#"
+                    className="flex items-center gap-x-4 px-6 py-3 text-sm/6 font-semibold text-white hover:bg-white/5"
+                  >
+                    <img
+                      alt=""
+                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      className="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10"
+                    />
+                    <span className="sr-only">Your profile</span>
+                    <span aria-hidden="true">Tom Cook</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+        <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+          <button
+            type="button"
+            onClick={() => setSidebarOpen(true)}
+            className="-m-2.5 p-2.5 text-gray-400 hover:text-white lg:hidden"
+          >
+            <span className="sr-only">Open sidebar</span>
+            <Bars3Icon aria-hidden="true" className="size-6" />
+          </button>
+          <div className="flex-1 text-sm/6 font-semibold text-white">Dashboard</div>
+          <a href="#">
+            <span className="sr-only">Your profile</span>
+            <img
+              alt=""
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              className="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10"
+            />
+          </a>
+        </div>
+
+        {/*MAIN CONTENT*/}
+        <main className="py-10 lg:pl-72">
+          <div className="px-4 sm:px-6 lg:px-8">
+            {/* Your content */}
+            {navigation[0].current && <Landing />}
+            {navigation[1].current && <TopRated />}
+            {navigation[2].current && <Trending />}
+            {navigation[3].current && <Favorites />}
+          </div>
+        </main>
+      </div>
     </>
   );
 }
